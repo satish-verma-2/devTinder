4f6f13499c63167d75376abae34c91e56760e891
satish <satish@greymedia.co>
2026-09-01 15:01:11 +0530

    edit profile api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
-
-
-profileRouter.get("/profile", userAuth, async (req, res)=>{
-   try {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
+  try {
     // const cookies = req.cookies;
     // const token = cookies.token;
     // if(!token){
@@ -21,13 +20,42 @@ profileRouter.get("/profile", userAuth, async (req, res)=>{
     // console.log("cookie", cookies);
     // console.log("user", user);
     const user = req.user;
-    if(!user){
-        throw new Error("user does not exists");
+    if (!user) {
+      throw new Error("user does not exists");
     }
     res.status(200).send(user);
-    } catch(err){
-        res.status(400).send("Error is : "+ err.message);
+  } catch (err) {
+    res.status(400).send("Error is : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      // res.status(400).send("invalid data");
+      throw new Error(" invalid edit requests");
     }
+    const loggedinUser = req.user;
+
+    // this is also the way but right now will not use this
+    // const updatedUser = {
+    //   ...req.user,
+    //   ...req.body,
+    // };
+
+    console.log(loggedinUser);
+    Object.keys(req.body).forEach((key) => {
+      loggedinUser[key] = req.body[key];
+    });
+    await loggedinUser.save();
+    res.json({
+      status: 200,
+      message: `${loggedinUser.firstName}, your profile data updated succesfully`,
+      data: loggedinUser,
+    });
+  } catch (err) {
+    res.status(400).send("Error " + err.message);
+  }
 });
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,17 +1,37 @@
-const validator = require('validator');
+const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
-    if (!firstName || !lastName) {
-        throw new Error("Name is not valid!");
-    } else if (!validator.isEmail(email)) {
-        throw new Error(" email is not valid");
-    } else if (!validator.isStrongPassword(password)) {
-        throw new Error("Please Enter a strong password!")
-    }
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid!");
+  } else if (!validator.isEmail(email)) {
+    throw new Error(" email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please Enter a strong password!");
+  }
+};
 
-}
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "email",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((fields) =>
+    allowedEditFields.includes(fields),
+  );
 
-module.exports = 
-    {validateSignUpData}
+  //In my openion below code seems more understandable to me but akshay teach me above
+  //   const isEditAllowed = allowedEditFields.every((field) =>
+  //   Object.keys(req.body).includes(field)
+  // );
+  return isEditAllowed;
+};
+
+module.exports = { validateSignUpData, validateEditProfileData };
